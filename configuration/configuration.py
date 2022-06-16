b456ecd14f97104b072e89e4bf697b6632267370
eliya <eliya.finkelman@gmail.com>
2022-06-17 01:00:26 +0300

    SubtitlesCreator: created configuration file with paths for creating the subtitles

---
+++ configuration/configuration.py
@@ -0,0 +1,8 @@
+class FileConfig:
+    class Input:
+        input_video_path = r"input\video.mp4"
+        input_subtitle_path = r"input\subtitles.txt"
+
+    class Output:
+        output_video_path = r"output\output.mp4"
+        output_subtitle_path = r"output\subtitles.srt"
